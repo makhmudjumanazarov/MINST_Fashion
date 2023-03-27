b6710d7ed040ba5e3aa1aa76384236a3dd3db7cb
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-28 00:27:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -65,17 +65,11 @@ if st.button('Predict'):
         img_array = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
         st.image(img_array)
         img_array = cv2.resize(img_array.astype('uint8'), (28, 28))
-        st.write(img_array.shape)
-        img_array = img_array.reshape(1, 28, 28)
-        st.write(img_array.shape)
         st.write(img_array)
-        img_array = img_array.transpose((1, 2, 0))
-        st.write(img_array.shape)
-        st.write(img_array)
-#         predict = model_load.predict(img_array.reshape(1, 28, 28))    
-#         predicts = np.argmax(predict, axis=1)
-#         output_text = predicts[0]
-#         font_size = "36px"
-#         st.markdown("<h3 style='text-align: left; color: black; font-size: {};'>{}</h3>".format(font_size, output_text), unsafe_allow_html=True)
+        predict = model_load.predict(img_array.reshape(1, 28, 28))    
+        predicts = np.argmax(predict, axis=1)
+        output_text = predicts[0]
+        font_size = "36px"
+        st.markdown("<h3 style='text-align: left; color: black; font-size: {};'>{}</h3>".format(font_size, labels[output_text]), unsafe_allow_html=True)
     except:
         pass
