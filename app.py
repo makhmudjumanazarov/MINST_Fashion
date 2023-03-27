6c9c8bceac67a0a409455a9983b77305d437cbed
Makhmud Jumanazarov <95900131+makhmudjumanazarov@users.noreply.github.com>
2023-03-28 00:39:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,55 +11,19 @@ def fe_data(df):
     df = df / 255.
     return df
 
-def get_predictions_load(X_test):
-    predictions = model_load.predict(X_test)    
-    predictions = np.argmax(predictions, axis=1)
-    return predictions
-
 model_load = load_model('model')
 labels = ['T-shirt/top','Trouser','Pullover','Dress','Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 st.title('Fashion MNIST Image Recognizer')
 st.write('classes')
 st.write(labels, horizontal = True)
-# genre = st.radio(
-#     "choose one of the two",
-# ('Draw by hand', 'Upload image'))
 
-# if genre == 'Draw by hand':
-#     st.markdown('''
-#     Try to write a image!
-#     ''')
-#     SIZE = 192
-#     canvas_result = st_canvas(
-#         fill_color='#000000',
-#         stroke_width=10,
-#         stroke_color='#FFFFFF',
-#         background_color='#000000',
-#         width=SIZE,
-#         height=SIZE,
-#         drawing_mode="freedraw",
-#         key='canvas')
-
-#     if canvas_result.image_data is not None:
-#         img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
-#         rescaled = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
-# else:
 img_file_buffer = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 if img_file_buffer is not None:
     image = Image.open(img_file_buffer)
     img_array = np.array(image)
         
 if st.button('Predict'):
-#     try:
-#         test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#         prediction = model_load.predict(fe_data(test_x).reshape(1, 28, 28))    
-#         predictions = np.argmax(prediction, axis=1)
-#         output_text = predictions[0]
-#         font_size = "36px"
-#         st.markdown("<h3 style='text-align: left; color: black; font-size: {};'>{}</h3>".format(font_size, labels[output_text]), unsafe_allow_html=True)
-#     except:
-#         pass
     try:
         img_array = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
         st.image(img_array)
